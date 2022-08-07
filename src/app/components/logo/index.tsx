e2184dac9fb549240af53b357e49dcfe2293d6c0
krutulkamil <krutulkamil@onet.pl>
2022-08-07 22:02:25 +0200

    footer section

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -2,6 +2,12 @@ import React, {FunctionComponent} from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import CarLogoImg from "../../../assets/images/car-logo.png";
+import CarLogoDarkImg from "../../../assets/images/car-logo-dark.png";
+
+interface ILogoProps {
+    color?: "white" | "dark";
+    bgColor?: "white" | "dark";
+}
 
 const LogoContainer = styled.div`
     ${tw`
@@ -18,7 +24,8 @@ const LogoText = styled.div`
         text-black
         m-1
     `}
-`;
+    ${({color}: any) => (color === "white" ? tw`text-white` : tw`text-black`)}
+` as any;
 
 const Image = styled.div`
     width: auto;
@@ -34,13 +41,15 @@ const Image = styled.div`
     }
 `;
 
-const Logo: FunctionComponent = (): JSX.Element => {
+const Logo: FunctionComponent<ILogoProps> = (props): JSX.Element => {
+    const { color, bgColor } = props;
+
     return (
         <LogoContainer>
             <Image>
-                <img src={CarLogoImg} alt="logo"/>
+                <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg} alt="logo"/>
             </Image>
-            <LogoText>Yourcar.</LogoText>
+            <LogoText color={color || "dark"}>Yourcar.</LogoText>
         </LogoContainer>
     );
 };
