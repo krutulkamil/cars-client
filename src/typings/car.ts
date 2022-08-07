b54fc066c73c993b67763473187179e32205cbf2
krutulkamil <krutulkamil@onet.pl>
2022-08-07 21:30:28 +0200

    top cars carousel section

---
+++ src/typings/car.ts
@@ -0,0 +1,9 @@
+export interface ICar {
+    thumbnailScr: string;
+    name: string;
+    mileage: string;
+    gearType: string;
+    dailyPrice: number;
+    monthlyPrice: number;
+    gas: string;
+}
