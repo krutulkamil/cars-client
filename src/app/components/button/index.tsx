d74fed711bb0da51acf19bc1464ee07f31ef0b72
Kamil Krutul <k.krutul@cheil.com>
2022-08-07 21:30:28 +0200

    top cars carousel section

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -5,6 +5,7 @@ import tw from "twin.macro";
 interface IButtonProps {
     theme?: "filled" | "outlined";
     text: string;
+    className?: string;
 }
 
 const BaseButton = styled.button`
@@ -50,12 +51,12 @@ const FilledButton = styled(BaseButton)`
 `;
 
 const Button: FunctionComponent<IButtonProps> = (props): JSX.Element => {
-    const { theme, text } = props;
+    const { theme, text, className } = props;
 
     if (theme === "filled") {
-        return <FilledButton>{ text }</FilledButton>
+        return <FilledButton className={className}>{ text }</FilledButton>
     } else {
-        return <OutlinedButton>{ text }</OutlinedButton>
+        return <OutlinedButton className={className}>{ text }</OutlinedButton>
     }
 };
 
