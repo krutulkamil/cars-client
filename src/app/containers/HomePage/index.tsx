bc3560840af52b3be15070cea374259985e32651
Kamil Krutul <k.krutul@cheil.com>
2022-08-07 22:02:25 +0200

    footer section

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -8,6 +8,7 @@ import Marginer from "../../components/marginer";
 import BookingSteps from "./bookingSteps";
 import AboutUs from "./aboutUs";
 import TopCars from "./topCars";
+import Footer from "../../components/footer";
 
 const PageContainer = styled.div`
     ${tw`
@@ -33,6 +34,7 @@ const HomePage: FunctionComponent = (): JSX.Element => {
             <AboutUs />
             <Marginer direction="vertical" margin="8em" />
             <TopCars />
+            <Footer />
         </PageContainer>
     );
 };
