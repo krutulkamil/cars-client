a173f55d1d93170546aab62c2efff53d2cc62399
Kamil Krutul <k.krutul@cheil.com>
2022-08-07 14:00:23 +0200

    top section setup

---
+++ src/app/components/bookCard/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const BookCard: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>dsaasddsa</div>
+    );
+};
+
+export default BookCard;
